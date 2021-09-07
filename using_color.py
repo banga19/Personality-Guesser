c281efd7c5a34bc8495efda70fc7c18c25ee0a3b
banga19 <62591509+banga19@users.noreply.github.com>
2021-09-07 10:30:34 +0300

    Personality Guesser
    Takes in a user's Favorite color and gives out their character as output

---
+++ using_color.py
@@ -0,0 +1,181 @@
+
+
+name  = input("what is your name: ")
+
+
+print("What a beautiful name you have here!!")
+
+print( f"Nice to meet you {name} . My name is ALLY and i am going to help you know what your personality is ." )
+
+fav_color1 = "green"
+fav_color2 = "blue"
+fav_color3 = "red"
+fav_color4 = "purple"
+fav_color5 = "orange"
+fav_color6 = "grey"
+fav_color7 = "pink"
+fav_color8 = "black"
+fav_color9 = "white"
+fav_color10 = "brown"
+fav_color11 = "yellow"
+fav_color12 = "silver"
+fav_color13 = "gold"
+fav_color14 = "lilac"
+fav_color15 = "violet"
+
+color = input(f"What is your favourite color, {name} ?")
+
+if color == fav_color1 :
+    print(""""
+You are: practical, loyal, consistent, generous and kind.
+You are  also prone to putting lots of importance on money and security.
+Although you might think loving green means you love nature, according to color
+psychology, those who love green desperately want to feel secure in both 
+their finances and relationships. To them, how they are viewed by others is 
+their greatest concern, and they really want to be seen as successful,
+wealthy, and an important person in their social circles. They're also social
+ butterflies and are probably on a diet as we speak.""")
+
+elif color == fav_color2:
+    print(""""
+Blue is the most popular prettiest color in the world.
+You are: Calm, cool, and able to keep things more even keel than most.
+Blue, the color of the ocean, is often associated with feelings of serenity,
+ so those who love it are often able to find peace and tranquility in places where others do not.
+ People who love to wear blue are said to be easy to get along with, reliable,
+  and, for lack of a better word, lovable.""")
+
+elif color == fav_color3:
+    print("""" You are: Bold,loud, boisterous, adventurous.
+You want to make a lasting impression.
+Studies have also found that both genders are more attracted to a person in red than any other color.
+Although scientists aren't exactly sure why, they think it may have to do with the fact that when someone blushes they
+turn red and there's some sort of animalistic, sexual attraction there. People whose favorite color is red are usually
+aware the effect it has, and are extroverted and confident enough to take advantage of it.
+Interestingly, it's also been found that a preference for the color red increases
+ for women when they've reached peak fertility in their menstrual cycle.
+""")
+
+elif color == fav_color4:
+    print("""
+You are: A wee bit off, but it your own special way.
+People who are drawn to purple are sort of hippy-like in the way they impractically tackle the world with their utopian ideals.
+ They tend to favor mysticism to reality, and prefer to see the world through violet-colored glasses in a way that can be difficult for those around them.
+ It's probably best that they set up a Tarot card reading shop stat, and just get it over with already.""")
+
+elif color == fav_color5:
+    print("""
+You are: Friendly, easy going, and probably a theater major.
+Those who adore orange want to be the center of attention, tend to be flamboyant in nature, and are not likely to have too many serious thoughts. They're friendly,
+and people like that — until the overdose of flamboyancy kicks in, when friends tend to run.
+According to Faber Birren, who studies color, those with a favorite color of orange are not as likely to marry,
+ and if they do, their "marriage will be one of light affection." Yikes.""")
+
+elif color ==fav_color6:
+    print("""
+You are: Afraid of commitment.
+Somewhere between black and white, grey is the color that just can't commit,
+and the same goes for those who love it. From the perspective of color 
+psychology, grey is emotionless, boring, detached, and indecisive.
+Those who say their favorite color is grey don't tend to have any major
+likes or dislikes. They can take or leave most things, and, on a whole,
+lack the passion that comes with loving a 'real' color.""")
+
+elif color == fav_color7:
+    print("""
+You are: A little naive, soft, loving, sensitive and warm sheltered, and have delicate sensibilities.
+you are always giving moe than what you are receiving. You have a child-like side which makes you even more lovable.
+Those who favor pink do so because it's an escape from the reality of
+impending adulthood. They're childlike in the way they interact with others,
+and hope to be able to pull off red, pink's older sister,
+when they finally grow up ― failing to realize they've been an adult for a
+long time now.
+ Perfect example of the pink mentality: Girls' Shoshanna Shapiro.""")
+
+elif color == fav_color8:
+    print("""
+You are: Part moody, part sophisticated, strong-willed and determined.
+you want power and prestige and you are not afraid go after what you want.
+Sometimes you can be demanding and dictatorial and can have difficulty letting other people in.
+Like Angelina Jolie, those who call black their favorite
+color tend to be complicated realists with a need for control. Authority and independence are important to you.
+Their realistic view of the world keeps their pessimism and moodiness in check,
+while their simple approach to always keeping things dark and dreary
+makes them appear far more sophisticated than they might actually be.
+""")
+
+elif color == fav_color9:
+    print("""
+You are: Innocent. (Or at least you think you are.)
+White embraces innocence, virgins, and is the favorite
+color of F. Scott Fitzgerald's character, Daisy Buchanan, 
+a woman who wanted to create a perception that wasn't entirely true.
+Those who favor white want to convince others (and themselves)
+of a purity they likely just don't have. They're also really meticulous in their appearance,unlike the rest of us who would never be
+able to keep our whites white.""")
+
+elif color == fav_color10:
+    print("""
+You are: Simple and comfortable.
+Those who love brown have no need for extravagance.
+They like things as they are, and can do without any complications that come
+with change. They're reliable, frugal, direct, and being spur of the moment is
+just not in their personality.""")
+
+elif color == fav_color11:
+    print("""
+You are: A happy idealist who is underestimated far too often, curious and enjoy learning new things.
+You are intelligent and laser focused. You know how to live a good life but hide your 
+fears and sadness behind a smile.
+It takes a certain type of personality to prefer yellow to all the other colors.
+It involves a level of optimism that most people over the age of 10 
+just don't have. Because of this optimism, and the idealism that comes along with it, people who love
+yellow sometimes come off as whacky or aloof, but that's just because the 
+moody folks are straight-up jealous.""")
+
+elif color == fav_color12:
+    print("""
+You are a thinker; introspective and imaginative.
+You see things from a different angle and you are not afraid 
+to voice your eccentric opinions. You have a lot of life experience and you are
+always happy to share your stories and wisdom with those around you.
+You are very resourceful and powerful at the same time. People are drawn to you because of your self-assurance and your captivating aura.
+Life seems to work out your way and you always have a sincere smile on your face.""")
+
+elif color == fav_color13:
+    print("""
+Just like your color, you want to be noticed and number one all the time. You are always looking for something to make you even more special and for opportunities to shine and lead.
+You are super confident and charismatic that attention naturally follows you. You know who you are and what you want, but you are also genuine and giving.
+You are authentic and trustworthy and you always seek quality over quantity. """)
+
+elif color == fav_color14:
+    print("""
+Your feelings run deep and you can be quite sensitive to hurtful comments from
+others, although you would never show it. Being a personality color purple, you have a peaceful and tranquil quality and a quiet dignity about you.
+People are drawn to your charismatic and alluring energy.""")
+
+elif color == fav_color15:
+    print("""
+You are: compassionate, sensitive,understanding and supportive.
+You tend to think of others before yourself.
+People who are drawn to violet are sort of hippy-like in the way they impractically tackle the world with their utopian ideals.
+You are the person people come to for help- being needed motivates you but sometimes people take advantage of you.
+It's probably best that they set up a Tarot card reading shop stat, and just get it over with already.""")
+
+
+else:
+    print("sorry, we dont have that color here!. Come back next time")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
